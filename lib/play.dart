d5db575a8a5246481d9bbc107ef396432814799a
芭乐 <1372432518@qq.com>
2021-04-28 18:29:11 +0800

    播放界面设置

--- lib/play.dart
+++ lib/play.dart
@@ -1,8 +1,12 @@
 import 'dart:ui';
 
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:freemusic_flutter/musicModel.dart';
 import 'package:freemusic_flutter/networkManager.dart';
+import 'package:freemusic_flutter/playManager.dart';
+import 'dart:math' as math;
 
 PlayWidget sharedPlay = PlayWidget();
 
@@ -19,10 +23,13 @@ class _PlayWidgetState extends State<PlayWidget> {
   List musicList = sharedPlay.musicList;
   MusicModel model = sharedPlay.model;
   String picUrl = '';
+  String musicTotalTime = '00:00';
+  String musicCurrentTime = '00:00';
 
+  int totalTime = 0;
+  int currentTime = 0;
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     if (model != null) {
       getPicUrl();
@@ -36,12 +43,31 @@ class _PlayWidgetState extends State<PlayWidget> {
     if (artistStr == null) {
       artistStr = '';
     }
+
+    PlayerManger().player.onAudioPositionChanged.listen((event) {
+      musicCurrentTime = transformToTime(event.inMilliseconds);
+      currentTime = event.inMilliseconds;
+      setState(() {});
+    });
   }
 
   void getPicUrl() async {
     picUrl =
         await NetworkManager().requestPicUrlWithId(int.parse(model.pic_id));
+    musicTotalTime =
+        transformToTime((await PlayerManger().player.getDuration()));
+    totalTime = await PlayerManger().player.getDuration();
+    musicCurrentTime =
+        transformToTime((await PlayerManger().player.getCurrentPosition()));
+    currentTime = await PlayerManger().player.getCurrentPosition();
     setState(() {});
+  }
+
+  String transformToTime(int time) {
+    var duration = Duration(milliseconds: time);
+    List<String> parts = duration.toString().split(':');
+    String minteStr = parts[2].toString().substring(0, 2);
+    return '${parts[1]}:${minteStr}';
   }
 
   @override
@@ -73,17 +99,49 @@ class _PlayWidgetState extends State<PlayWidget> {
           Positioned(
             child: DecoratedBox(
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(150),
-                color: Colors.black,
+                  borderRadius: BorderRadius.circular(150),
+                  color: Colors.grey,
+                  image: DecorationImage(
+                      image: picUrl != ''
+                          ? NetworkImage(picUrl)
+                          : AssetImage('images/music.png'))),
+              child: Transform.rotate(
+                angle: 0.02,
               ),
-              child: picUrl == ''
-                  ? Image.asset('images/music.png')
-                  : Image.network(picUrl),
             ),
             height: 300,
             width: 300,
+            top: 110,
           ),
           Positioned(
+            child: Text(musicCurrentTime),
+            bottom: 114,
+            left: 10,
+          ),
+          Positioned(
+            child: SizedBox(
+              height: 5,
+              child: LinearProgressIndicator(
+                backgroundColor: Colors.grey[200],
+                valueColor: AlwaysStoppedAnimation(Colors.blue),
+                value: totalTime == 0 ? 0 : currentTime / totalTime,
+              ),
+            ),
+            bottom: 120,
+            left: 50,
+            right: 50,
+          ),
+          Positioned(
+              child: Text(
+                PlayerManger().player.state == AudioPlayerState.PLAYING
+                    ? musicTotalTime
+                    : '00:00',
+                textDirection: TextDirection.rtl,
+              ),
+              right: 10,
+              bottom: 114),
+          Positioned(
+            height: 100,
             child: Row(
               children: [
                 Text('循环'),
@@ -105,7 +163,7 @@ class _PlayWidgetState extends State<PlayWidget> {
                 Text('列表')
               ],
             ),
-            bottom: 50,
+            bottom: 20,
           )
         ],
       ),
